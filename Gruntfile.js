45ed37e1e7feaf70a97ebdb3b74c1bc31e74368a
icymorn <icymorn@gmail.com>
2014-07-01 18:01:49 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      },
+      dist: {
+        src: ['./lib/main.js', './lib/lexer.js'],
+        dest: './dist/code-lighter.js'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['lib/*.js']
+      },
+      task: ['concat']
+    }
+  });
+
+  grunt.event.on('watch', function(action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('cat', ['concat']);
+};
---
+++ dist/code-lighter.js
@@ -0,0 +1,7 @@
+/*! blog - v0.0.1 - 2014-07-01 */var lighter = {};
+;var lighter = lighter || {};
+lighter.lexer = lighter.lexer || {};
+
+lexer['js'] = function () {
+	
+};
